2e0ea1a47f21573acc37eb6ee6a766f34bafa8e7
BcA9HuK <126610490+BcA9HuK@users.noreply.github.com>
2026-01-18 17:51:50 +0700

    Update vite.config.github.ts

--- vite.config.github.ts
+++ vite.config.github.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/puzzle-generator44/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/puzzle-generator4/' : '/',
   server: {
     allowedHosts: true,
   },
